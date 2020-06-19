dd28854416592fd6fb2d3d0af1352a4d8704c827
Colin <colinrwelsh@gmail.com>
2020-06-19 13:12:12 -0400

    updated sources

--- scrape_data.R
+++ scrape_data.R
@@ -1,6 +1,4 @@
-###############
-## Packages
-###############
+# Packages & Init Setup ---------------------------------------------------
 
 pkgs <- c("tidyverse", "ggplot2", "ggfortify", "gganimate", "magick")
 installed_packages <- pkgs %in%
@@ -10,37 +8,45 @@ if (any(installed_packages == FALSE)) {
 }
 invisible(lapply(pkgs, library, character.only = TRUE))
 
-###############
-## Get Data
-###############
-
-confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
-
-US_confirmed <- confirmed %>% filter(`Country/Region` == "US")
+# Get Data ----------------------------------------------------------------
 
 today <- Sys.Date()
 today <- format(Sys.Date(), format("%m/%d/%y"))
 
+confirmed <-
+  read_csv(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+  )
+US_confirmed <- confirmed %>% filter(`Country/Region` == "US")
+
 ## x.colnames <- seq(as.Date("3/1/20", format = "%m/%d/%Y"), today, by = 1)
+# sum(US_confirmed$`4/5/20`) ## today's date
+# 
+# f.US_data <- "cases.csv"
+# US_data <- read_csv(file = f.US_data) ## from google sheet
+# 
+# if (US_data$Date[1] == "3/1/2020") {
+#   US_data$Date <- as.Date(US_data$Date, format = "%m/%d/%y")
+#   write.csv(US_data, file = f.US_data, row.names = FALSE)
+# }
+# 
+# US_data[,4] <- NULL
+# view(US_data)
+# US_data_rev <- US_data %>% filter(Date >= Sys.Date()-14 & Date <= Sys.Date()+7)
+# view(US_data_rev)
 
-sum(US_confirmed$`4/5/20`) ## today's date
+cdc_weekly_deaths <-
+  read_csv("https://data.cdc.gov/resource/3yf8-kanr.csv")
 
-f.US_data <- "cases.csv"
-US_data <- read_csv(file = f.US_data) ## from google sheet
+covid_tracking <- "https://covidtracking.com/api/v1/"
 
-if (US_data$Date[1] == "3/1/2020") {
-  US_data$Date <- as.Date(US_data$Date, format = "%m/%d/%y")
-  write.csv(US_data, file = f.US_data, row.names = FALSE)
-}
+us_daily <-
+  read_csv(paste0(covid_tracking,"us/daily.csv"))
 
-US_data[,4] <- NULL
-view(US_data)
-US_data_rev <- US_data %>% filter(Date >= Sys.Date()-14 & Date <= Sys.Date()+7)
-view(US_data_rev)
+states_daily <-
+  read_csv(paste0(covid_tracking,"states/daily.csv"))
 
-###############
-## Chart
-###############
+# Chart -------------------------------------------------------------------
 
 g1 <- US_data_rev %>% ggplot(
   aes(x = Date, y = Confirmed, color = Type)) +
@@ -64,9 +70,7 @@ g1 +
   transition_reveal(Date) +
   view_follow()
 
-###############
-
-###############
+# Chart -------------------------------------------------------------------
 
 g1 <- US_confirmed %>% ggplot(
   aes(x = Date, y = Confirmed, color = Type)) +
